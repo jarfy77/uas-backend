f02efe44e6101e34f4519bd59faa300d23537557
jarfy77 <pacenakal666@gmail.com>
2023-11-30 20:28:21 +0900

    first commit

---
+++ controllers/Products.js
@@ -0,0 +1,144 @@
+import Product from "../models/ProductModel.js";
+import User from "../models/UserModel.js";
+import {Op} from "sequelize";
+
+export const getProducts = async (req, res) =>{
+    try {
+        let response;
+        if(req.role === "admin"){
+            response = await Product.findAll({
+                attributes:['uuid','name','jabatan','gaji'],
+                include:[{
+                    model: User,
+                    attributes:['name','email']
+                }]
+            });
+        }else{
+            response = await Product.findAll({
+                attributes:['uuid','name','jabatan','gaji'],
+                where:{
+                    userId: req.userId
+                },
+                include:[{
+                    model: User,
+                    attributes:['name','email']
+                }]
+            });
+        }
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
+}
+
+export const getProductById = async(req, res) =>{
+    try {
+        const product = await Product.findOne({
+            where:{
+                uuid: req.params.id
+            }
+        });
+        if(!product) return res.status(404).json({msg: "Data tidak ditemukan"});
+        let response;
+        if(req.role === "admin"){
+            response = await Product.findOne({
+                attributes:['uuid','name','jabatan','gaji'],
+                where:{
+                    id: product.id
+                },
+                include:[{
+                    model: User,
+                    attributes:['name','email']
+                }]
+            });
+        }else{
+            response = await Product.findOne({
+                attributes:['uuid','name','jabatan','gaji'],
+                where:{
+                    [Op.and]:[{id: product.id}, {userId: req.userId}]
+                },
+                include:[{
+                    model: User,
+                    attributes:['name','email']
+                }]
+            });
+        }
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
+}
+
+export const createProduct = async(req, res) =>{
+    const {name, jabatan,gaji} = req.body;
+    try {
+        await Product.create({
+            name: name,
+            jabatan: jabatan,
+            gaji: gaji,
+            userId: req.userId
+        });
+        res.status(201).json({msg: "Product Created Successfuly"});
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
+}
+
+export const updateProduct = async(req, res) =>{
+    try {
+        const product = await Product.findOne({
+            where:{
+                uuid: req.params.id
+            }
+        });
+        if(!product) return res.status(404).json({msg: "Data tidak ditemukan"});
+        const {name, jabatan,gaji} = req.body;
+        if(req.role === "admin"){
+            await Product.update({name, jabatan,gaji},{
+                where:{
+                    id: product.id
+                }
+            });
+        }else{
+            if(req.userId !== product.userId) return res.status(403).json({msg: "Akses terlarang"});
+            await Product.update({name, jabatan,gaji},{
+                where:{
+                    [Op.and]:[{id: product.id}, {userId: req.userId}]
+                }
+            });
+        }
+        res.status(200).json({msg: "Product updated successfuly"});
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
+}
+
+export const deleteProduct = async(req, res) =>{
+    try {
+        const product = await Product.findOne({
+            where:{
+                uuid: req.params.id
+            }
+            //fucsiotn it this a product that has alre
+        });
+        if(!product) return res.status(404).json({msg: "Data tidak ditemukan"});
+        const {name,jabatan,gaji} = req.body;
+        if(req.role === "admin"){
+            await Product.destroy({
+                where:{
+                    id: product.id
+                }
+            });
+        }else{
+            if(req.userId !== product.userId) return res.status(403).json({msg: "Akses terlarang"});
+            await Product.destroy({
+                where:{
+                    [Op.and]:[{id: product.id}, {userId: req.userId}]
+                }
+            });
+        }
+        res.status(200).json({msg: "Product deleted successfuly"});
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
+}
